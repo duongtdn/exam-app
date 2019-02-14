f04444dd41742c95234061563aee0f0b6b7854be
duongtdn <duongtdn@gmail.com>
2019-02-14 07:57:43 +0000

    background of html page is white now

--- src/api/exam/exam-page.js
+++ src/api/exam/exam-page.js
@@ -2,7 +2,7 @@
 
 const html = ({ title, script, data }) => `
 <!DOCTYPE html>
-<html class="w3-light-grey">
+<html>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1,  shrink-to-fit=no">
