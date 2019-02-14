b78161116afe95c0c26fa6a9ac454ee854ad06c9
duongtdn <duongtdn@gmail.com>
2019-02-14 08:04:24 +0000

    change Skipped Quiz to Saved Quiz and change completion text to 15%

--- src/clients/Page/Widgets/StatusBar.js
+++ src/clients/Page/Widgets/StatusBar.js
@@ -10,7 +10,7 @@ export default class StatusBar extends Component {
     return(
       <div className="" style={{margin: '8px 0'}}>
         <div className="w3-cell w3-pale-green w3-padding" style={{display: 'inline-block', textAlign: 'center', margin: '4px 8px 0 0'}}>
-          <div className="w3-text-green w3-small"> Completion (20%) </div>
+          <div className="w3-text-green w3-small"> Completion (15%) </div>
           <div className="w3-text-green w3-large" style={{fontWeight: 'bold', marginTop: '6px'}}> 15/100 </div>
         </div>  
         <div className="w3-cell w3-pale-yellow w3-padding" style={{display: 'inline-block', textAlign: 'center', margin: '4px 8px 0 0'}}>
@@ -18,7 +18,7 @@ export default class StatusBar extends Component {
           <div className="w3-text-yellow w3-large" style={{fontWeight: 'bold', marginTop: '6px'}}> 01:05:56 </div> 
         </div> 
         <div className="w3-cell w3-pale-blue w3-padding  w3-cell-top w3-hide-small" style={{display: 'inline-block', textAlign: 'left', margin: '4px 0', width: 'calc(100% - 300px)'}}>
-          <div className="w3-text-blue w3-small">Skipped Quiz </div>   
+          <div className="w3-text-blue w3-small">Saved Quiz </div>   
           <div className="">
             {
               [...Array(100).keys()].map( i => (
@@ -30,7 +30,7 @@ export default class StatusBar extends Component {
           </div>            
         </div>     
         <div className="w3-cell w3-pale-blue w3-padding  w3-hide-medium w3-hide-large" style={{display: 'inline-block', textAlign: 'left', margin: '4px 0', width: '100%'}}>
-          <div className="w3-text-blue w3-small"> Skipped Quiz <i className = "fa fa-sort-down w3-large" style={{margin: '0 8px'}} /> </div>   
+          <div className="w3-text-blue w3-small"> Saved Quiz <i className = "fa fa-sort-down w3-large" style={{margin: '0 8px'}} /> </div>   
           <div className="w3-show">
             <hr />
             {
