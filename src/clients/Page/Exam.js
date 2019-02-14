b26e6a80098c5edba2b51e9034ca6a03fdc3d145
duongtdn <duongtdn@gmail.com>
2019-02-14 04:11:08 +0000

    add Date

--- src/clients/Page/Exam.js
+++ src/clients/Page/Exam.js
@@ -17,9 +17,10 @@ export default class Exam extends Component {
   }
 
   componentWillMount() {
+    const today = getToday();
     const { course, type } = getCourseIdFromHref(window.location.href)
     if (course) {
-      this.setState({ course, type })
+      this.setState({ course, type, today })
     } else {
       this.setState({ error: '400 Bad Request' })
     }
@@ -33,10 +34,11 @@ export default class Exam extends Component {
         <div>
           <Header />
           <div className="w3-container">
-            <p> 
+            <div style={{marginTop: '10px'}}> 
               <label className="w3-text-blue w3-large" style={{fontWeight:'bold'}}> <span className="w3-text-black"> Course: </span>{this.state.course} ({this.state.type}) </label> <br />
-              <label className="w3-text-grey w3-small"> Date: </label>
-            </p>            
+              <label className="w3-text-grey w3-small"> {this.state.today}</label>
+            </div>      
+
           </div>
           {/* <button onClick={() => this.requestNewSession()}> New Session </button> */}
         </div>    
@@ -57,4 +59,13 @@ function getCourseIdFromHref(href) {
   const course= query.split('&')[0].split('=')[1]
   const type= query.split('&')[1].split('=')[1]
   return { course, type }
+}
+
+function getToday() {
+  const weekday  = ['Sunday', 'Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  const today = new Date()
+  const dd = today.getDate();
+  const mm = ("00" + (today.getMonth()+1)).slice(-2); //January is 0!
+  const yyyy = today.getFullYear();
+  return `${weekday[today.getDay()]} ${dd}-${mm}-${yyyy}`
 }
