64c10f1bed02fd6b79d96d23a985cbb9849c7c73
duongtdn <duongtdn@gmail.com>
2019-05-31 04:16:00 +0000

    move html func into seperated

--- src/api/exam/exam-page.js
+++ src/api/exam/exam-page.js
@@ -1,25 +1,8 @@
 "use trict"
 
-const html = ({ title, script, data }) => `
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1,  shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">  
-    <title>${title}</title>  
-    <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
-	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
-  </head>
-  <body>    
-    <div id="root"></div>
-    <script> var __data=${JSON.stringify(data)} </script>
-    ${script? `<script type="text/javascript" src="${script}" ></script>` : ''}    
-  </body>
-</html>
-`
+const { html } = require('../html')
 
-function redirect() {
+function reponse() {
   return function(req, res) {
     const testId = req.query.t
     if (testId) {
@@ -38,4 +21,4 @@ function redirect() {
   }
 }
 
-module.exports = redirect
+module.exports = reponse
