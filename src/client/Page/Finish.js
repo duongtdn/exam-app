afb03f47ca35b838647f27fa1dec5ff2c79c2344
duongtdn <duongtdn@gmail.com>
2019-06-05 04:17:52 +0000

    fix href link

--- src/client/Page/Finish.js
+++ src/client/Page/Finish.js
@@ -17,7 +17,7 @@ export default class Finish extends Component {
           <h3 className="w3-text-blue"> Test has been completed. Thank you for your participation</h3>
           <p> To access test result, click here </p>
           <p>
-            <a href={`/result/${this.props.resultId}`} className="w3-button w3-blue"> Test Result </a>
+            <a href={`/result?r=${this.props.resultId}`} className="w3-button w3-blue"> Test Result </a>
           </p>
         </div>
       </div>
