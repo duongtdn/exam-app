cf30de1ce6264f96d97959b66f2381450aafafb1
duongtdn <duongtdn@gmail.com>
2019-06-13 20:52:56 +0700

    add user info into Header

--- src/client/Page/Widgets/Header.js
+++ src/client/Page/Widgets/Header.js
@@ -2,6 +2,22 @@
 
 import React, { Component } from 'react'
 
+class UserSnipet extends Component {
+  render() {
+    const user = this.props.user
+    return (
+      <div className="w3-bar-item w3-border w3-round-large w3-white" style={{margin: '4px 0', padding: '3px 16px', cursor: 'pointer'}}>
+        <img className="w3-circle w3-border w3-border-white"style={{width: '35px'}} src="https://i1.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?resize=256%2C256&quality=100" />
+        {' '}
+        <span style={{marginRight: '4px'}}>{user.profile.displayName}</span>
+        {' '}
+        <i className="w3-text-dark-grey fa fa-ellipsis-v" />
+        {/* <button className="w3-button" onClick={evt => this.props.accountClient.signout()}> Logout </button> */}
+      </div>
+    )
+  }
+}
+
 export default class Header extends Component {
   constructor(props) {
     super(props)
@@ -14,9 +30,9 @@ export default class Header extends Component {
               Test Room
             </div>
             <div className={`w3-right w3-container ${this.props.endgame? '' : ' w3-hide'}`}>
-              <button className="w3-bar-item w3-button" onClick={this.props.endgame}> Finish </button>
+              {/* <button className="w3-bar-item w3-button" onClick={this.props.endgame}> Finish </button> */}
               {
-                this.props.user? <button className="w3-bar-item w3-button" onClick={evt => this.props.accountClient.signout()}> Logout </button> : null
+                this.props.user? <UserSnipet user={this.props.user} /> : null
               }
             </div>
         </div>
