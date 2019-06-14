e75218893762de36567414e714a08607457c339b
duongtdn <duongtdn@gmail.com>
2019-06-14 17:31:03 +0700

    minor change in ui

--- src/client/Page/Widgets/Header.js
+++ src/client/Page/Widgets/Header.js
@@ -10,7 +10,7 @@ class UserSnipet extends Component {
   render() {
     const user = this.props.user
     return (
-      <div className="w3-bar-item w3-round-large w3-dropdown-click" style={{margin: '4px 0', padding: '3px 16px', cursor: 'pointer'}}>
+      <div className="w3-bar-item w3-dropdown-click" style={{margin: '4px 0', padding: '3px 16px', cursor: 'pointer'}}>
         <span onClick={e => this.setState({show: !this.state.show})}>
           <img className="w3-circle w3-border w3-border-white"style={{width: '35px'}} src="https://i1.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?resize=256%2C256&quality=100" />
           {' '}
@@ -19,7 +19,7 @@ class UserSnipet extends Component {
           <i className="w3-text-dark-grey fa fa-ellipsis-v" />
         </span>
         {/* <button className="w3-button" onClick={evt => this.props.accountClient.signout()}> Logout </button> */}
-        <div className={`${this.state.show? 'w3-show': ''} w3-dropdown-content w3-bar-block w3-border`} style={{margin: '3px 0px 3px -16px'}}>
+        <div className={`${this.state.show? 'w3-show': ''} w3-dropdown-content w3-bar-block w3-border`} style={{margin: '3px 0px 3px -16px', minWidth: '131px'}}>
           <button className="w3-bar-item w3-button" onClick={this.props.endgame}> Finish Test </button>
           <hr />
           <button className="w3-bar-item w3-button" onClick={evt => this.props.accountClient.signout()}> Logout </button>
