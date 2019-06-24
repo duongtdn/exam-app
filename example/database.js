804f467ffb2c7cbbeb97bf59f1a8461287aba6a2
duongtdn <duongtdn@gmail.com>
2019-06-24 03:57:34 +0000

    fix quiz path in test data

--- example/database.js
+++ example/database.js
@@ -20,16 +20,16 @@ const Tests = [
       ],
       questions: [
         {
-          problem: 'choices.1', correctAnswers: {'$1': '/^false$/i', '$2': '/^true$/i', '$3': '/^false$/i'}, score: 10, section: 'sc1'
+          problem: 'choice-0.0', correctAnswers: {'$1': '/^false$/i', '$2': '/^true$/i', '$3': '/^false$/i'}, score: 10, section: 'sc1'
         },
         {
-          problem: 'dragdrop.1', correctAnswers: {'$1': {top: '/^310$/', left: '/^20$/'}, '$2': {top: '/^160$/', left: '/^30$/'}, '$3': {top: '/^70$/', left: '/^510$/'}}, score: 10, section: 'sc1'
+          problem: 'dragdrop-0.0', correctAnswers: {'$1': {top: '/^310$/', left: '/^20$/'}, '$2': {top: '/^160$/', left: '/^30$/'}, '$3': {top: '/^70$/', left: '/^510$/'}}, score: 10, section: 'sc1'
         },
         {
-          problem: 'dragdrop.2', correctAnswers: {'$1': {top: '/^310$/', left: '/^20$/'}, '$2': {top: '/^160$/', left: '/^30$/'}, '$3': {top: '/^70$/', left: '/^510$/'}}, score: 10, section: 'sc1'
+          problem: 'dragdrop-0.1', correctAnswers: {'$1': {top: '/^310$/', left: '/^20$/'}, '$2': {top: '/^160$/', left: '/^30$/'}, '$3': {top: '/^70$/', left: '/^510$/'}}, score: 10, section: 'sc1'
         },
         {
-          problem: 'text.1', correctAnswers: {'$1': '/^computer$/i', '$2': '/^2005$/i'}, score: 10, section: 'sc2'
+          problem: 'text-0.1', correctAnswers: {'$1': '/^computer$/i', '$2': '/^2005$/i'}, score: 10, section: 'sc2'
         },
       ]
     },
