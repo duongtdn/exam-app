1a96f9d0d1736e5620a58bae6d154522f0720491
duongtdn <duongtdn@gmail.com>
2019-06-25 07:44:04 +0700

    redesign ui

--- src/client/Page/EndPopup.js
+++ src/client/Page/EndPopup.js
@@ -2,15 +2,27 @@
 
 import React, { Component } from 'react'
 
+import storage from '../lib/storage'
+
 export default class EndPopup extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      submitted: storage.get(storage.SUBMITTEDKEY) || [],
+    }
+  }
+  componentDidMount() {
+    this._sHandler = storage.observe(storage.SUBMITTEDKEY, (data) => {
+      const submitted = data || []
+      this.setState({ submitted })
+    })
   }
   render() {
+    const quizzesLeft = this.props.totalQuizzes - this.state.submitted.length
     if (this.props.timeout) {
-      return this.renderIfTimeout()
+      return this.renderIfTimeout(quizzesLeft)
     } else {
-      return this.renderIfNotTimeout()
+      return this.renderIfNotTimeout(quizzesLeft)
     }
   }
   renderIfTimeout() {
@@ -18,28 +30,38 @@ export default class EndPopup extends Component {
       <div className="w3-modal" style={{ display: this.props.show ? 'block' : 'none' }}>
          <div className="w3-modal-content">
           <div className="w3-container w3-padding">
-            <h3> End Game </h3>
+            <span onClick={this.props.close} className="w3-button w3-display-topright w3-red">&times;</span>
+            <h4 className="w3-text-blue" style={{marginTop: '32px'}}> This test is ending </h4>
 
-            <p> You have run out of time. Submitting all left answers and finish... </p>
+            <p className="w3-text-dark-grey"> Submitting all left answers and finish. Please do not close the broswer </p>
 
           </div>
         </div>
       </div>
     )
   }
-  renderIfNotTimeout() {
+  renderIfNotTimeout(quizzesLeft) {
     return(
       <div className="w3-modal" style={{ display: this.props.show ? 'block' : 'none' }}>
          <div className="w3-modal-content">
           <div className="w3-container w3-padding">
-            <h3> End Game </h3>
+            <span onClick={this.props.close} className="w3-button w3-display-topright w3-red">&times;</span>
+            <h4 className="w3-text-blue" style={{marginTop: '32px'}}> Dou you really want to finish your test? </h4>
 
-            <p> There's still time. Dou you really one to finish your test </p>
+            {
+              quizzesLeft === 1 ?
+                <p className="w3-text-dark-grey"> 1 question is not answerred or submitted </p>
+                :  quizzesLeft > 1 ? <p className="w3-text-dark-grey"> {quizzesLeft} questions are not answerred or submitted </p>
+                : null
+            }
 
-            <br />
-            <div> <button className="w3-button w3-blue" onClick = {this.props.close}> Cancel </button> </div>
-            <br />
-            <div> <button className="w3-button w3-blue" onClick = {this.props.finish}> Submit all left answers & Finish </button> </div>
+            <hr />
+
+            <div className = "w3-right">
+              <button className="w3-button" onClick = {this.props.close}> Cancel </button>
+              <button className="w3-button w3-blue" onClick = {this.props.finish}> Finish </button>
+            </div>
+
           </div>
         </div>
       </div>
